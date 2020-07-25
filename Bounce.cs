dcdcf2c6da90eb49c7f48b92ba7e9b371db11156
Vansh Ranga <39514268+Vansh700@users.noreply.github.com>
2020-07-25 22:04:53 +0530

    Add files via upload

---
+++ Bounce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float bounce_speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * bounce_speed);
+        }
+    }
+}
---
+++ Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float speed = 20f;
+    float dirX;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        dirX = Input.acceleration.x * speed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -2.51f, 2.51f), transform.position.y);
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, 0f);
+    }
+}
